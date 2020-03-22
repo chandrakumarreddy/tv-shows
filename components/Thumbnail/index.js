06687dd4c9a62e9ae53c9d28b7f7580b9f429605
chandrakumarreddy <chandrapenugonda655@gmail.com>
2020-03-22 12:07:45 +0530

    add fallback image for thumbnail component

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,11 +1,15 @@
 import "./style.scss";
 
 export default function Thumbnail({ show }) {
-  const { image } = show;
+  const { name, image } = show;
   return (
     <div className="thumbnail">
-      <img src={image?.medium} alt={show.name} className="thumbnail__image" />
-      <h3 className="thumbnail__caption">{show.name}</h3>
+      <img
+        src={image?.medium ?? `http://via.placeholder.com/210x295?text=${name}`}
+        alt={show.name}
+        className="thumbnail__image"
+      />
+      <h3 className="thumbnail__caption">{name}</h3>
     </div>
   );
 }
