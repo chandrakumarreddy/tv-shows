b8121757f055cb05116bba49b47ecadc433cd6e4
chandrakumarreddy <chandrapenugonda655@gmail.com>
2020-03-22 16:30:02 +0530

    custom google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,10 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <style jsx>{`
-        body {
+        @import url("https://fonts.googleapis.com/css?family=Baloo+2&display=swap");
+        :global(body) {
           margin: 0;
+          font-family: "Baloo 2", cursive;
         }
         :global(ul) {
           padding: 0;
