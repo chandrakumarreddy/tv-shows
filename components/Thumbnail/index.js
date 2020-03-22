dbf1077f25f9e396ea870829b75eff61ce2e89af
chandrakumarreddy <chandrapenugonda655@gmail.com>
2020-03-22 12:45:15 +0530

    add routing to thumbnail

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,15 +1,22 @@
+import Link from "next/link";
 import "./style.scss";
 
 export default function Thumbnail({ show }) {
   const { name, image } = show;
   return (
-    <div className="thumbnail">
-      <img
-        src={image?.medium ?? `http://via.placeholder.com/210x295?text=${name}`}
-        alt={show.name}
-        className="thumbnail__image"
-      />
-      <h3 className="thumbnail__caption">{name}</h3>
-    </div>
+    <Link href="[country]/[showId]" as={`/us/${show.id}`}>
+      <a>
+        <div className="thumbnail">
+          <img
+            src={
+              image?.medium ?? `http://via.placeholder.com/210x295?text=${name}`
+            }
+            alt={show.name}
+            className="thumbnail__image"
+          />
+          <h3 className="thumbnail__caption">{name}</h3>
+        </div>
+      </a>
+    </Link>
   );
 }
--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import axios from "axios";
 import Thumbnail from "../../components/Thumbnail";
 
@@ -5,15 +6,47 @@ export default function Country({ shows }) {
   const renderShows = () => {
     return shows.map((_show, index) => <Thumbnail {..._show} key={index} />);
   };
-  return <ul>{renderShows()}</ul>;
+  return (
+    <ul className="tvshows-grid">
+      {renderShows()}{" "}
+      <style jsx>{`
+        .tvshows-grid {
+          list-style-type: none;
+          padding: 0;
+          margin: 0;
+          display: grid;
+          grid-template-columns: repeat(4, 1fr);
+          gap: 10px;
+        }
+        @media only screen and (max-width: 600px) {
+          .tvshows-grid {
+            grid-template-columns: repeat(2, 1fr);
+          }
+        }
+      `}</style>
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+          }
+        `}
+      </style>
+    </ul>
+  );
 }
 
 Country.getInitialProps = async context => {
   const country = context.query.country || "US";
-  const response = await axios.get(
-    `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
-  );
+  let shows = [];
+  try {
+    const response = await axios.get(
+      `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
+    );
+    shows = response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
   return {
-    shows: response.data
+    shows
   };
 };
