a119b9d0d4c1cc31b7bb6dfbec1e87cc2c5d8254
chandrakumarreddy <chandrapenugonda655@gmail.com>
2020-03-24 23:37:36 +0530

    change cast url from http to https

--- utils/urls.js
+++ utils/urls.js
@@ -1,3 +1,3 @@
 export const SIGNIN = "https://iwallet-api.herokuapp.com/api/auth/signin";
 export const SIGNUP = "https://iwallet-api.herokuapp.com/api/auth/signup";
-export const GET_CAST = "http://api.tvmaze.com/people";
+export const GET_CAST = "https://api.tvmaze.com/people";
